57666103bb324afadd72d5d4224d8145569d275f
Manav378 <manupatel5814@gmail.com>
2026-01-19 08:31:05 +0530

    i

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,10 +3,11 @@ import Navbar from '../components/Navbar.jsx'
 import Header from '../components/Header.jsx'
 const Home = () => {
   return (
-    <div className='flex flex-col items-center bg-black justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
-     <Navbar/>
-     <Header/>
-    </div>
+   <div className='flex flex-col items-center bg-black justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
+  <Navbar />
+  <Header />
+</div>
+
   )
 }
 
