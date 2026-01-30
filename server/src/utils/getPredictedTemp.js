91187bcd861f918a589c6ed29806ffa58c7056a8
Manav378 <manupatel5814@gmail.com>
2026-01-31 01:36:40 +0530

    predict

--- server/src/utils/getPredictedTemp.js
+++ server/src/utils/getPredictedTemp.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export const getTemperaturePrediction = async (req, res) => {
   try {
     // Production me python API ka URL environment variable se use karo
-    const PYTHON_API_URL = process.env.PYTHON_API_URL || "https://aerothermal-fan-ai-ml.vercel.app/predict";
+const PYTHON_API_URL = process.env.PYTHON_API_URL || "https://aerothermal-fan-ai-ml.vercel.app/api/predict";
+
 
     const response = await axios.get(PYTHON_API_URL);
     console.log(res.data)
