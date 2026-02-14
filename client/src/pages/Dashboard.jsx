137039779c3f66f669abc8e7fe994580fdd82894
Manav378 <manupatel5814@gmail.com>
2026-02-15 00:00:38 +0530

    humidity error

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -8,7 +8,7 @@ import RpmGauge from "../components/RpmCard.jsx";
 import PWMSliderCard from "../components/pwm.jsx";
 import AutoModeCard from "../components/automode.jsx";
 import CurrentPWMCard from "../components/Currentpwm.jsx";
-import Humidity from "../components/humidity.jsx";
+import Humidity from "../components/Humidity.jsx";
 import Heatindex from "../components/heatindex.jsx";
 import { translations } from "../Theme/translation.js";
 
